a15d2b56b742ee78c92c180622299474ce4f8c7d
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-01-17 18:37:21 -0300

    iniciando deploy pm2 instalado

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors");
+require("dotenv/config");
+
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/appError");
 const uploadConfig = require("./configs/uploads");
@@ -37,7 +39,7 @@ app.use( (error, request, response, next) =>{
   );
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, ()=> console.log(`Server is running on Port ${PORT}`) );
 
 // métodos https: get, post, put, patch, delete 
